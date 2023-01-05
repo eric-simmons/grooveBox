81dd6fdfa3ce14c36dadfa5756a44c0e56aa0394
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-01-05 15:47:58 -0600

    cmajor

--- kick.js
+++ kick.js
@@ -22,6 +22,25 @@ let notes = []
 let noteLength = ["2n", "8n", "16n"]
 let step = {}
 
+//scales
+let CMajor = []
+const Major = [0, 2, 4, 5, 7, 9, 11, 12];
+let startingNote = 60
+for (let i = 0; i < Major.length; i++) {
+    CMajor.push(Major[i] + startingNote);
+}
+
+// setPitch = () => {
+//     return (CMajor[Math.floor(Math.random() * CMajor.length)])
+// }
+
+// setStep = (bar, sixteenth) => {
+//     return {
+//         time: setTime(bar, sixteenth),
+//         note: setPitch(bar),
+//         velocity: setVelocity()
+//     }
+// }
 
 //get the notes from slider and translate to midi note then push to notes array
 const getNotes = () => {
@@ -47,7 +66,10 @@ sequencerSliders.forEach((slider, index) => {
 //toggle active step buttons
 stepBtns.forEach((button, index) => {
     button.addEventListener('click', (event) => {
+
+        sequencerSliders[index].classList.toggle("activeSlider")
         event.target.classList.toggle("activeStep")
+
     })
 })
 
@@ -60,7 +82,7 @@ loopEndSlider.addEventListener('input', (event) => {
     kickSequence.set({
         loopEnd: +loopEndSlider.value
     })
-loopEndValue.innerHTML = `LoopEnd ${loopEndSlider.value}`
+    loopEndValue.innerHTML = `LoopEnd ${loopEndSlider.value}`
     stepBtns.forEach((button, index) => {
         if (index >= loopEndSlider.value) {
             button.classList.add("lastStep")
@@ -87,14 +109,14 @@ const visualSequence = new Tone.Sequence((time) => {
         const time = Tone.Time("4n").toSeconds() * 1000 / 2
         let currentBtn = stepBtns[currentStep]
         let currentSlider = sequencerSliders[currentStep]
-        currentSlider.classList.add("active")
+        currentSlider.classList.add("currentSlider")
         currentBtn.classList.add("playHead")
-        
+
         setTimeout(() => {
             currentBtn.classList.remove("playHead")
         }, time)
         setTimeout(() => {
-            currentSlider.classList.remove("active")
+            currentSlider.classList.remove("currentSlider")
         }, time)
     }, time)
 }, notes).start(0)
@@ -117,7 +139,6 @@ const kick = new Tone.MembraneSynth({
 ).toDestination()
 
 const kickSequence = new Tone.Sequence((time) => {
-
     let currentStep = Math.floor(kickSequence.progress * kickSequence.loopEnd)
     if (stepBtns[currentStep].matches('.activeStep')) {
         kick.triggerAttackRelease(notes[currentStep], noteLength[currentStep], time)
